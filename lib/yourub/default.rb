12059e93918d95fd5235fb061aee05c98f5b7b8f
Davide Prati <lastexxit@gmail.com>
2015-02-24 20:07:01 +0100

    remove bug

--- lib/yourub/default.rb
+++ lib/yourub/default.rb
@@ -1,6 +1,2 @@
-#Yourub::Config.load!(File.join("config", "yourub.yml"), 'yourub_defaults')
-
 logger = Logger.new(STDOUT)
-logger.level = Logger.const_get(Yourub::Config.log_level.upcase)
-
 Yourub.logger = logger
--- lib/yourub/version.rb
+++ lib/yourub/version.rb
@@ -1,6 +1,6 @@
 module Yourub
   MAJOR = 2
   MINOR = 0
-  PATCH = 0
+  PATCH = 1
   VERSION = [MAJOR, MINOR, PATCH].compact.join('.')
 end
